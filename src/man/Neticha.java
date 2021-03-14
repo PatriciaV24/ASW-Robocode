179722e2e7006891858b266e42e8a87958e7ea90
manelsa1912 <manelsa1912@gmail.com>
2021-03-14 19:30:05 +0000

    engrish

--- src/man/Neticha.java
+++ src/man/Neticha.java
@@ -10,36 +10,36 @@ import java.util.Random;
 
 
 /**
- * O robo Neticha é a máquina de destruiçao criada com as seguintes caracteristicas:
+ * -Neticha is a destruction machine that consists in the following:
  *  
- * 	-O movimeto baseia-se na deteçao de disparos do adversrio, o que gera um movimento aleatorio nosso: 
- * 		ou andamos em zigzag ou numa circunferencia em volta do adversario, mantendo sempre uma
- * 		distancia de segurança;
- *  
- * 	-A arma consiste no conceito de wavebullet. Wavebullet e um sistema de deteçao de padroes de movimento
- * 		com pesos nos angulos possiveis para onde posso disparar, e gradualmente reunir informaçao.
+ * 	-It moves when it detects a enemy bullet coming. It can move in a zigzag, or in a circle around the
+ * 	enemy.
+ *  -The gun is based on the wavebullet algoritm. It creates a list that adjusts the possible angles of 
+ * 	the gun, and shoots with the best angle in the moment.
  *
  * @author Manuel Sá
  * @author Patrícia Vieira
  */
 public class Neticha extends AdvancedRobot{
-	double energiaEnimigo = 100;
-	int dirMovimento = 1;
+	double EnemyEnergy = 100;
+	int moveDirection = 1;
 	int flagshoot=0;
 	int firePow=1;
 	double fireSpeed=0; 
 	
 
-	//lista que guarda as possibilidades da arma para disparar 
+	//list where are the gunwaves shot at the moment
 	ArrayList<GunWave> gunWaves=new ArrayList<GunWave>();
 	
-	//array dos angulos possiveis da arma
+	//array that has the possible angles of the gun
 	static double [] gunAngles=new double[16];
 
 	public void run() {
 	
-		//coloca cores e separa o radar e a arma do movimento
+		
 		setColors(Color.white, Color.white, Color.white);
+		
+		//makes the body,radar and gun move separetly
 		setAdjustGunForRobotTurn(true);
 		setAdjustRadarForGunTurn(true);
 
@@ -54,10 +54,10 @@ public class Neticha extends AdvancedRobot{
 
 	public void onScannedRobot(ScannedRobotEvent e) {
 		
-		//variaçao da vida do inimigo
-		double changeInEnergy = energiaEnimigo-e.getEnergy();
 		
-		//Radar do robo
+		double changeInEnergy = EnemyEnergy-e.getEnergy();
+		
+		//Robot Radar
 		double posInimigo= getHeadingRadians() +e.getBearingRadians();
 		double radar=Utils.normalRelativeAngle(posInimigo-getRadarHeadingRadians());
 		double extraT=Math.min(Math.atan(36.0/e.getDistance()),Rules.RADAR_TURN_RATE_RADIANS);
@@ -66,38 +66,38 @@ public class Neticha extends AdvancedRobot{
 
 		verificarlimites();
 
-		//Movimento do Robo
+		//Robot movement
 		Random ran = new Random();
 		int r= ran.nextInt(2);
 		
-		if(r==0) dirMovimento=-dirMovimento;
+		if(r==0) moveDirection=-moveDirection;
 			
 
 		if (changeInEnergy>0 &&changeInEnergy<=3) {
 			setColors(Color.black, Color.black, Color.black);
 			flagshoot=1;
 			if(e.getDistance()<200){
-				setAhead(-(e.getDistance()/4+200)*dirMovimento);
-				setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*dirMovimento);
+				setAhead(-(e.getDistance()/4+200)*moveDirection);
+				setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*moveDirection);
 			}else{
-				setAhead((e.getDistance()/4+25)*dirMovimento);
+				setAhead((e.getDistance()/4+25)*moveDirection);
 			}
 
-			dirMovimento =-dirMovimento;	
+			moveDirection =-moveDirection;	
 			
 			if(e.getDistance()>450){
-                setAhead((e.getDistance()/4+25)*dirMovimento);
-				setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*dirMovimento);
+                setAhead((e.getDistance()/4+25)*moveDirection);
+				setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*moveDirection);
             }
 		}else{
 			if(e.getDistance()<300 && flagshoot==0){
-				setAhead(-(e.getDistance()/4+200)*dirMovimento);
-				setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*dirMovimento);
+				setAhead(-(e.getDistance()/4+200)*moveDirection);
+				setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*moveDirection);
 			}
 		}
 
 	
-		//Arma do robo
+		//robot gun
 		if(e.getDistance()<150) 
 			firePow=3;
 		else{
@@ -107,14 +107,11 @@ public class Neticha extends AdvancedRobot{
 				firePow=1;
 			}
 		
-		
-		//prepara o disparo
 		if(getGunHeat()==0){
             fireSpeed=20-3*firePow;
 			logFiringWave(e);
         }
 		
-		//verificar a lista das waves
 		checkFiringWaves(project(new Point2D.Double(getX(),getY()),e.getDistance(),posInimigo));
 		
 		setTurnGunRightRadians(Utils.normalRelativeAngle(posInimigo-getGunHeadingRadians())+gunAngles[8+(int)(e.getVelocity()*Math.sin(e.getHeadingRadians()-posInimigo))]);
@@ -124,16 +121,16 @@ public class Neticha extends AdvancedRobot{
 
 		verificarlimites();
 
-		energiaEnimigo = e.getEnergy();
+		EnemyEnergy = e.getEnergy();
 	}
 
 	public void onHitWall(HitWallEvent e){
-		dirMovimento=-dirMovimento;
+		moveDirection=-moveDirection;
 	}
 
 	public void onHitByBullet(HitByBulletEvent e){
-		setAhead(-300*dirMovimento);
-		setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*dirMovimento);
+		setAhead(-300*moveDirection);
+		setTurnRightRadians(e.getBearingRadians()+Math.PI/2-Math.PI/6*moveDirection);
 	}
 
 	public void onWin(WinEvent e){
@@ -145,44 +142,44 @@ public class Neticha extends AdvancedRobot{
 		if(getX()<=36){
 			if(getHeadingRadians()>= Math.PI){
 				setAhead(-200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 
 			}else{
 				setAhead(200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 			}
 		}
 		if(getY()<=36){
 			if(getHeadingRadians()>= Math.PI/2 && getHeadingRadians()<= (3*Math.PI)/2){
 				setAhead(-200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 			}else{
 				setAhead(200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 			}
 		}
 		if(getX()>=getBattleFieldWidth()-36){
 			if(getHeadingRadians()>= 0 && getHeadingRadians()<=Math.PI){
 				setAhead(-200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 			}else{
 				setAhead(200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 			}
 		}
 		if(getY()>=getBattleFieldHeight()-36){
 			if(getHeadingRadians()<= Math.PI/2 || getHeadingRadians()>=(3*Math.PI)/2){
 				setAhead(-200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 			}else{
 				setAhead(200);
-				setTurnRightRadians(Math.PI/4*dirMovimento);
+				setTurnRightRadians(Math.PI/4*moveDirection);
 			}
 		}
 
 	}
 
-	 //Esta classe contem as informaçoes de uma preparaçao de disparo
+	 //the class that has all the information of a shot
 	class GunWave{ 
 		double speed;
 		Point2D.Double origin;
@@ -191,7 +188,7 @@ public class Neticha extends AdvancedRobot{
 		double startTime;
 	}	
 
-	//verifica se uma preparaçao falhou ...se falhou remove o da lista e ajusta o angulo
+	//verifies if the shot of the gunwave missed...if so, thw gunwave is removed from the list and the angle is adjusted
 	public void checkFiringWaves(Point2D.Double ePos){
         GunWave w;
         for(int i=0;i<gunWaves.size();i++){
@@ -203,7 +200,7 @@ public class Neticha extends AdvancedRobot{
         }
     }
 
-	//adiciona uma possivel preparaçao da arma a lista
+	//adds a gunwave to the list
 	public void logFiringWave(ScannedRobotEvent e){
         GunWave w=new GunWave();
         w.absBearing=e.getBearingRadians()+getHeadingRadians();
@@ -214,7 +211,7 @@ public class Neticha extends AdvancedRobot{
         gunWaves.add(w);
     }
 	
-	//este metodo permite nos saber uma posiçao a partir de um angulo e outra coordenada 
+	//this method allows us to know a coordenate from an angle and another coordenate 
 	public Point2D.Double project(Point2D.Double origin,double dist,double angle){
 	    return new Point2D.Double(origin.x+dist*Math.sin(angle),origin.y+dist*Math.cos(angle));
 	}
